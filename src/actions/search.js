b682179dd2845cb9ae2d8bbe91fd30d9a8d5e761
Edwin-Baranov <edwin.baranov@gmail.com>
2021-10-06 12:33:48 -0700

    Implemented Actions

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,14 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+  console.log(q);
   //TODO:  Write an asynchronous action to handle a video search!
+  return (dispatch) => {
+    searchYouTube({key: YOUTUBE_API_KEY, query: q}, (videos) => {
+      console.log(videos);
+      dispatch(changeVideoList(videos));
+    });
+  };
 };
 
 export default handleVideoSearch;
